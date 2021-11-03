8e0ea2ff411a83268a84d7e9b2782660397b2661
Hisymak <kozten@seznam.cz>
2021-11-03 21:41:26 +0100

    Exploding barrel building should not deal damage also when it's captured

--- src/fixes/no-building-damage-when-sold.asm
+++ src/fixes/no-building-damage-when-sold.asm
@@ -1,9 +1,9 @@
 %include "macros/patch.inc"
 
-; Exploding barrel-type building should not make damage when it is sold
+; Exploding barrel-type building should not make damage when it is sold or captured
 hack 0x0049BD4C, 0x0049BD51 ; UpdateBuilding
     mov eax, [esi+28h] ; get building flags
-    test eax, 4 ; check if selling flag is set
+    test eax, 0x1000004 ; check if selling or infiltrated flag is set
     jnz 0x49BD87 ; skip call of DamageTiles
     mov al, [0x6D60EE] ; instruction replaced by the long jump  
     jmp hackend
