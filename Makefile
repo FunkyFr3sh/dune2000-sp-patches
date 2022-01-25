2edde7f0733a95ac93b2679e8a0dab85e269aa51
Hisymak <kozten@seznam.cz>
2022-01-25 22:18:17 +0100

    Crates can be picked up by a carryall dropping a unit on them

--- Makefile
+++ Makefile
@@ -47,6 +47,7 @@ OBJS            ?= callsites.o \
 	src/extended-maps/guard-mode-radius.o \
 	src/extended-maps/always-show-radar.o \
 	src/extended-maps/tile-damage.o \
+	src/extended-maps/pickup-crate-on-landing.o \
 	\
 	src/event-system/event-core.o \
 	src/event-system/event-conditions.o \
---
+++ src/extended-maps/pickup-crate-on-landing.asm
@@ -0,0 +1,29 @@
+%include "macros/patch.inc"
+%include "patch.inc"
+
+; Crates can be picked up by a carryall dropping a unit on them
+hack 0x00498865, 0x0049886B ; UpdateUnit
+    and ebx, 0FFh ; instruction replaced by the long jump
+    push ecx
+    push edx
+    push ebx ; side_id
+    push edi ; unit
+    call Mod__PickupCrate
+    add esp, 8
+    pop edx
+    pop ecx
+    jmp hackend
+
+hack 0x0049E5A9, 0x0049E5AF ; UnitDeliver
+    mov al, [edi+54h] ; instruction replaced by the long jump
+    mov [esi+54h], al ; instruction replaced by the long jump
+    mov eax, [esp+28h]
+    push ecx
+    push edx
+    push eax ; side_id
+    push esi ; unit
+    call Mod__PickupCrate
+    add esp, 8
+    pop edx
+    pop ecx
+    jmp hackend
