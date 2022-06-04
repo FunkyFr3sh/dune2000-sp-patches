0795c413f219597edf63859bb9db24f1df6da6db
Hisymak <kozten@seznam.cz>
2022-06-04 21:29:55 +0200

    Increase .AUD sound size limit to 512 kilobytes

--- src/extended-maps/sound-size-limit.asm
+++ src/extended-maps/sound-size-limit.asm
@@ -2,7 +2,7 @@
 %include "macros/datatypes.inc"
 %include "inc/patch.inc"
 
-; Increase sound size limit to 512 kilobytes
+; Increase WAV inside SOUND.RS size limit to 512 kilobytes
 @PATCH 0x0046EEFA ; ISampleManager__Allocate
     dd 0x80000
 @ENDPATCH
@@ -20,5 +20,10 @@
 @ENDPATCH
 
 @PATCH 0x0046F00D ; ISampleManager__LoadSampleCache
+    dd 0x80000
+@ENDPATCH
+
+; Increase AUD file size limit to 512 kilobytes
+@PATCH 0x0046FB94 ; SoundLoop
     dd 0x80000
 @ENDPATCH
