2b98d44854b853cb185664c1086e292174f0aed7
Hisymak <kozten@seznam.cz>
2024-01-09 20:48:36 +0100

    Fix spice blooms exploding by timer not taking custom properties into account

--- src/extended-maps/crates.asm
+++ src/extended-maps/crates.asm
@@ -11,3 +11,15 @@ hack 0x0049D326, 0x0049D394 ; DamageTiles
     call HitCrate
     add esp, 4
     jmp hackend
+
+; Overwrite the code which takes care of exploding large spice bloom by itself after timer has expired
+hack 0x0044EAE8, 0x0044EAF4 ; ProcessCrates
+    mov edx, esi
+    sub edx, 2 ; Pointer to crate
+    push 0 ; side_id
+    push 0 ; unit
+    push 10 ; crate_type
+    push edx ; crate
+    call HandleSpiceBloomCrate
+    add esp, 16
+    jmp hackend
