0f99834eaea398f81ea628361cfd146a8e9e008a
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-07-31 08:05:06 +0200

    update test.cmd

--- test.cmd
+++ test.cmd
@@ -10,26 +10,16 @@ if exist dune2000.exe exit 0
 set PATH=C:\win-builds-patch-32\bin
 gmake WWDEBUG=1
 
-del "C:\Spiele\Dune2000\1\dune2000-spawn.exe"
-if exist "C:\Spiele\Dune2000\1\dune2000-spawn.exe" exit 0
+del "C:\Games\Dune2000_MP_Test\Dune1\dune2000-spawn.exe"
+if exist "C:\Games\Dune2000_MP_Test\Dune1\dune2000-spawn.exe" exit 0
 
-del "C:\Spiele\Dune2000\2\dune2000-spawn.exe"
-if exist "C:\Spiele\Dune2000\2\dune2000-spawn.exe" exit 0
+del "C:\Games\Dune2000_MP_Test\Dune2\dune2000-spawn.exe"
+if exist "C:\Games\Dune2000_MP_Test\Dune2\dune2000-spawn.exe" exit 0
 
-del "C:\Spiele\Dune2000\3\dune2000-spawn.exe"
-if exist "C:\Spiele\Dune2000\3\dune2000-spawn.exe" exit 0
+copy /Y dune2000.exe "C:\Games\Dune2000_MP_Test\Dune1\dune2000-spawn.exe"
+copy /Y dune2000.exe "C:\Games\Dune2000_MP_Test\Dune2\dune2000-spawn.exe"
 
-del "C:\Spiele\Dune2000\4\dune2000-spawn.exe"
-if exist "C:\Spiele\Dune2000\4\dune2000-spawn.exe" exit 0
-
-copy /Y dune2000.exe "C:\Spiele\Dune2000\1\dune2000-spawn.exe"
-copy /Y dune2000.exe "C:\Spiele\Dune2000\2\dune2000-spawn.exe"
-copy /Y dune2000.exe "C:\Spiele\Dune2000\3\dune2000-spawn.exe"
-copy /Y dune2000.exe "C:\Spiele\Dune2000\4\dune2000-spawn.exe"
-
-cd "C:\Spiele\Dune2000"
-start LagSim2 C:\Spiele\Dune2000\1\dune2000-spawn.exe C:\Spiele\Dune2000\2\dune2000-spawn.exe
-cd "C:\Spiele\Dune2000\3"
+cd "C:\Games\Dune2000_MP_Test\Dune1"
 start dune2000-spawn.exe -SPAWN
-cd "C:\Spiele\Dune2000\4"
+cd "C:\Games\Dune2000_MP_Test\Dune2"
 start dune2000-spawn.exe -SPAWN
